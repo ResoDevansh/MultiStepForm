6cbcb36fdcd4b7d3edc9f43fcdd4619492c28ccb
ResoDevansh <sachandevansh2000@gmail.com>
2023-01-31 21:58:14 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import pic from "./assets/images/bg-sidebar-desktop.svg";
 
 function App() {
+  const navitems = [
+    {
+      step: "Step 1",
+      value: "Your info",
+    },
+    {
+      step: "Step 2",
+      value: "Select plan",
+    },
+    {
+      step: "Step 3",
+      value: "Add-ons",
+    },
+    {
+      step: "Step 4",
+      value: "Summary",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Navbar imgUrl={pic}>
+        {navitems.map((item) => {
+          return <>
+            <Heading>{item.step}</Heading>
+            <Content>{item.value}</Content>
+          </>;
+        })}
+      </Navbar>
+    </Container>
   );
 }
 
+const Container = styled.div`
+  height: 70.5vh;
+  width: 55vw;
+  margin: auto;
+  margin-top: calc(100vh / 7);
+  border-radius: 1em;
+  background-color: white;
+  overflow: hidden;
+`;
+
+const Navbar = styled.div`
+  border: 1px solid black;
+  background-image: url(${(props) => props.imgUrl});
+  background-repeat: no-repeat;
+  background-position: 100% 100%;
+  height: 66.7vh;
+  width: 16.7vw;
+  border-radius: 1em;
+  margin-top: calc((3.6vh) / 2);
+  margin-left: 0.8em;
+`;
+
+const Heading = styled.div`
+  border: 1px solid red;
+`;
+const Content = styled.div`
+  border: 1px solid green;
+`;
 export default App;
